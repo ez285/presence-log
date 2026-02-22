c132be812aca1b98628648f5a48f34d2a11c5054
ez285 <ezervas.zemcogroup@gmail.com>
2026-02-22 20:58:38 +0200

    Add files via upload

---
+++ google_sheet.py
@@ -0,0 +1,115 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    pass
+import gspread as gs
+from google.oauth2.service_account import Credentials
+import streamlit as sl
+from datetime import date
+
+SCOPES = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
+]
+
+@sl.cache_resource
+def get_worksheets():
+    creds = Credentials.from_service_account_info(sl.secrets['gcp_service_account'], scopes=SCOPES)
+    gc = gs.authorize(creds)
+    wbk = gc.open_by_key(sl.secrets['sheet_id'])
+    return wbk.worksheet('Presence Log'), wbk.worksheet('Companies'), wbk.worksheet('People')
+    
+class _Sheets:
+    def __init__(self) -> None:
+        self.PresenceLog:gs.Worksheet
+        self.Companies:gs.Worksheet
+        self.People:gs.Worksheet
+
+class _Data:
+    def __init__(self) -> None:
+        self.PresenceLog:list[dict[str, int|float|str]]
+        self.Companies:list[dict[str, int|float|str]]
+        self.People:list[dict[str, int|float|str]]
+
+class _Search_Company:
+    def ByID(self, id:int) -> list[dict[str, int|float|str]]: # type: ignore
+        pass
+
+    def ByName(self, name:str) -> list[dict[str, int|float|str]]:  # type: ignore
+        pass
+
+class _Search_Person:
+    def ByID(self, id:int) -> list[dict[str, int|float|str]]: # type: ignore
+        pass
+
+    def ByName(self, kwargs:dict[str,str]) -> list[dict[str, int|float|str]]: # type: ignore
+        pass
+
+    def ByCompany(self, companyName:str) -> list[dict[str, int|float|str]]: # type: ignore
+        pass
+
+class _Search:
+    def __init__(self) -> None:
+        self.Company = _Search_Company()
+        self.Person = _Search_Person()
+
+class PresenceLog:
+    def __init__(self) -> None:
+        self.Sheets = _Sheets()
+        self.Sheets.PresenceLog, self.Sheets.Companies, self.Sheets.People = get_worksheets()
+        self.Data = _Data()
+        self.Data.PresenceLog = self.Sheets.PresenceLog.get_all_records()
+        self.Data.Companies = self.Sheets.Companies.get_all_records()
+        self.Data.People = self.Sheets.People.get_all_records()
+        self.Search = _Search()
+        self.Search.Company.ByID = self._SearchCompanyByID
+        self.Search.Company.ByName = self._SearchCompanyByName
+        self.Search.Person.ByID = self._SearchPersonByID
+        self.Search.Person.ByName = self._SearchPersonByName
+        self.Search.Person.ByCompany = self._SearchPersonByCompany
+    
+    def LogPresence(self, data:list[dict[str, int|float|str]]) -> None:
+        if list(data[0].keys()) != list(self.Data.PresenceLog[0].keys()):
+            raise ValueError('Given titles do not match the expected ones')
+        self.Sheets.PresenceLog.append_rows([list(itm.values()) for itm in data], insert_data_option=gs.utils.InsertDataOption.insert_rows, table_range='A1')
+    
+    def AddCompanies(self, data:list[dict[str, int|float|str]]) -> list[int]:
+        if self.Data.Companies[0].keys() - data[0].keys() != {'Company ID'}:
+            raise ValueError('Given titles do not match the expected ones')
+        ids = [itm['Company ID'] for itm in self.Data.Companies]
+        idStart:int = 1 if not ids else max(ids) + 1 # type: ignore
+        idsNew = [idStart + i for i in range(0, data.__len__())]
+        self.Sheets.Companies.append_rows([[idNew, itm['Company Name'], itm['Role']] for idNew, itm in zip(idsNew, data)], insert_data_option=gs.utils.InsertDataOption.insert_rows, table_range='A1')
+        return idsNew
+    
+    def AddPeople(self, data:list[dict[str, int|float|str]]) -> list[int]:
+        if self.Data.People[0].keys() - data[0].keys() != {'Person ID'}:
+            raise ValueError('Given titles do not match the expected ones')
+        ids = [itm['Person ID'] for itm in self.Data.People]
+        idStart:int = 1 if not ids else max(ids) + 1 # type: ignore
+        idsNew = [idStart + i for i in range(0, data.__len__())]
+        self.Sheets.People.append_rows([[idNew, itm['Company ID'], itm['First Name'], itm['Last Name'], '', '', '', True] for idNew, itm in zip(idsNew, data)], insert_data_option=gs.utils.InsertDataOption.insert_rows, table_range='A1')
+        return idsNew
+    
+    def _SearchCompanyByID(self, id:int) -> list[dict[str, int|float|str]]:
+        return [itm for itm in self.Data.Companies if itm['Company ID'] == id]
+    
+    def _SearchCompanyByName(self, name:str) -> list[dict[str, int|float|str]]:
+        return [itm for itm in self.Data.Companies if itm['Company Name'] == name]
+    
+    def _SearchPersonByID(self, id:int) -> list[dict[str, int|float|str]]:
+        return [itm for itm in self.Data.People if itm['Person ID'] == id]
+    
+    def _SearchPersonByName(self, kwargs:dict[str,str]) -> list[dict[str, int|float|str]]:
+        if kwargs.keys() - {'First Name', 'Last Name'} or all([itm not in kwargs.keys() for itm in ['First Name', 'Last Name']]):
+            raise ValueError('Given titles do not match the expected ones')
+        res = self.Data.People
+        for condition in kwargs.items():
+            res = [itm for itm in res if itm[condition[0]] == condition[1]]
+        return res
+    
+    def _SearchPersonByCompany(self, companyName:str) -> list[dict[str, int|float|str]]:
+        if companyName not in [itm['Company Name'] for itm in self.Data.Companies]:
+            raise ValueError('Given company name does not exist')
+        companyID = self.Search.Company.ByName(companyName)[0]['Company ID']
+        return [itm for itm in self.Data.People if itm['Company ID'] == companyID]
---
+++ streamlit_app.py
@@ -0,0 +1,131 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    pass
+from enum import Enum, auto
+import streamlit as sl
+from datetime import date
+from google_sheet import PresenceLog
+
+@sl.cache_resource
+def GetPresenceLog() -> PresenceLog:
+    return PresenceLog()
+pL = GetPresenceLog()
+
+class StreamlitMode(Enum):
+    NameInputStandard = auto()
+    NameInputNewCompany = auto()
+    NameInputNewPeople = auto()
+    InOut = auto()
+    FullListToday = auto()
+    InToday = auto()
+
+def ShowDateCompany() -> None:
+    sl.session_state.setdefault('selectedDate', date.today())
+    sl.date_input('Date', label_visibility='visible', format='DD/MM/YYYY', key='selectedDate')
+    sl.selectbox('Company name', sorted([itm['Company Name'] for itm in pL.Data.Companies]) + ['Add New'], label_visibility='visible', key = 'selectedCompany')
+
+def ShowPersonellInput(companyName:str) -> None:
+    left, middle, right = sl.columns([4, 4, 1], vertical_alignment='bottom')
+    with left:
+        sl.text_input('First Name', label_visibility='visible', key='firstName')
+    with middle:
+        sl.text_input('Last Name', label_visibility='visible', key='lastName')
+    with right:
+        sl.button('Add', use_container_width=True, on_click=lambda companyName=companyName:AddNames(companyName))
+    sl.markdown('**** Names added ****')
+    sl.text('\n'.join(['\t'.join([itm2.__str__() for itm2 in itm]) for itm in sl.session_state.addedNames]))
+
+def ShowNamesForCompany() -> None:
+    selected = []
+    for person in sorted(pL.Search.Person.ByCompany(sl.session_state.selectedCompany), key=lambda itm: f'{itm["First Name"]} {itm["Last Name"]}'):
+        if sl.checkbox(f'{person["First Name"]} {person["Last Name"]}', key=f'person_{person["Person ID"]}'):
+            selected.append(person['Person ID'])
+    sl.session_state.selectedNameIDs = selected
+
+def ShowSubmitButton() -> None:
+    sl.button('Submit', on_click=Submit)
+
+def Submit() -> None:
+    # Firstly add the names that already exist in the tables
+    if sl.session_state.selectedNameIDs:
+        pL.LogPresence([{
+            'Project Code':0, 'Project Name':'', 'Company ID':pL.Search.Company.ByName(sl.session_state.selectedCompany)[0]['Company ID'], # type: ignore
+            'Person ID':itm, 'In':sl.session_state.selectedDate, 'Out':sl.session_state.selectedDate
+        } for itm in sl.session_state.selectedNameIDs])
+    # If additional names exist, add them now
+    if sl.session_state.addedNames:
+        # Get the given company names and find which are not already included in the table
+        addedNamesCompaniesUnique = set(itm[1] for itm in sl.session_state.addedNames)
+        existingCompanies = [itm['Company Name'] for itm in pL.Data.Companies]
+        companyNamesToAdd = [itm for itm in addedNamesCompaniesUnique if itm not in existingCompanies]
+        # If there are company names not included in the table, add them
+        newCompanyIDs = {}
+        if companyNamesToAdd:
+            newCompanyIDs = pL.AddCompanies([{'Company Name':itm, 'Role':''} for itm in companyNamesToAdd])
+            newCompanyIDs = dict(zip(companyNamesToAdd, newCompanyIDs))
+        # Inevitably, there are people to add, so add them as well
+        peopleToAdd = [{'Company ID':newCompanyIDs[itm[1]] if itm[1] in newCompanyIDs else pL.Search.Company.ByName(itm[1])[0]['Company ID'],
+                       'First Name':itm[2], 'Last Name':itm[3]
+                    } for itm in sl.session_state.addedNames]
+        newPeopleIDs = pL.AddPeople(peopleToAdd)
+        pL.LogPresence([{'Project Code':0, 'Project Name':'', 'Company ID':peopleToAdd[i]['Company ID'], 'Person ID':newPeopleIDs[i],
+                         'In':sl.session_state.addedNames[i][0], 'Out':sl.session_state.addedNames[i][0]
+                        } for i in range(0, newPeopleIDs.__len__())])
+    # clear state variables and rerun from the beginning
+    for itm in list(sl.session_state.keys()):
+        if itm.startswith('person_'): # type:ignore
+            del sl.session_state[itm]
+    sl.session_state.selectedNameIDs = []
+    sl.session_state.addedNames = []
+    sl.session_state.PreviousMode = sl.session_state.Mode
+    sl.session_state.Mode = StreamlitMode.NameInputStandard
+    GetPresenceLog.clear()
+    sl.rerun()
+
+
+def AddNames(companyName:str) -> None:
+    sl.session_state.addedNames.append([sl.session_state.selectedDate, companyName, sl.session_state.firstName, sl.session_state.lastName])
+    sl.session_state.firstName = ''
+    sl.session_state.lastName = ''
+
+# First thing, set up state variables
+if 'Mode' not in sl.session_state:
+    sl.session_state.Mode = StreamlitMode.NameInputStandard
+if 'PreviousMode' not in sl.session_state:
+    sl.session_state.PreviousMode = StreamlitMode.NameInputStandard
+if 'selectedNameIDs' not in sl.session_state:
+    sl.session_state.selectedNameIDs = []
+if 'addedNames' not in sl.session_state:
+    sl.session_state.addedNames = []
+
+# Second thing, reset state variable if mode changes
+if sl.session_state.Mode != sl.session_state.PreviousMode:
+    sl.session_state.selectedNameIDs = []
+    sl.session_state.addedNames = []
+    sl.session_state.PreviousMode = sl.session_state.Mode
+    
+if sl.session_state.Mode == StreamlitMode.NameInputStandard:
+    ShowDateCompany()
+    if sl.session_state.selectedCompany == 'Add New':
+        sl.session_state.PreviousMode = sl.session_state.Mode
+        sl.session_state.Mode = StreamlitMode.NameInputNewCompany
+        sl.rerun()
+    ShowNamesForCompany()
+    ShowPersonellInput(sl.session_state.selectedCompany)
+    ShowSubmitButton()
+elif sl.session_state.Mode == StreamlitMode.NameInputNewCompany:
+    ShowDateCompany()
+    if sl.session_state.selectedCompany != 'Add New':
+        sl.session_state.PreviousMode = sl.session_state.Mode
+        sl.session_state.Mode = StreamlitMode.NameInputStandard
+        sl.rerun()
+    sl.text_input('Company Name', label_visibility='visible', key='newCompany')
+    ShowPersonellInput(sl.session_state.newCompany)
+    ShowSubmitButton()
+elif sl.session_state.Mode == StreamlitMode.InOut:
+    pass
+elif sl.session_state.Mode == StreamlitMode.FullListToday:
+    pass
+elif sl.session_state.Mode == StreamlitMode.InToday:
+    pass
